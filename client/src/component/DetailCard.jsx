29998d2119f72018949d3e40ae06cb45e0e591bc
Haidarradam <haidaradam123@gmail.com>
2023-04-04 11:09:03 +0700

    New

--- client/src/component/DetailCard.jsx
+++ client/src/component/DetailCard.jsx
@@ -68,7 +68,7 @@ export default function Details(props) {
                 <SwiperSlide>
                   <div> */}
               <iframe
-                width="550"
+                width="530"
                 height="300"
                 src={films?.linkEps}
                 alt="Video"
